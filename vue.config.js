f8373298c3a565d39e200e76882d64f29b74eba5
xingen <335728491@qq.com>
2020-12-02 10:23:41 +0800

    打包优化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 module.exports = {
   publicPath: "/", //publicPath取代了baseUrl
   outputDir: "dist",
@@ -5,7 +6,19 @@ module.exports = {
   runtimeCompiler: true, //关键点在这  原来的 Compiler 换成了 runtimeCompiler
   // 调整内部的 webpack 配置。
   // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/webpack.md
-  chainWebpack: () => {},
+  chainWebpack: config => {
+    // 生产环境，开启js\css压缩
+    if (process.env.NODE_ENV === "production") {
+      config.plugin("compressionPlugin").use(
+        new CompressionWebpackPlugin({
+          test: /\.(js|css|less)$/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          minRatio: 0.8,
+          deleteOriginalAssets: true // 删除源文件
+        })
+      );
+    }
+  },
   configureWebpack: () => {},
   // 配置 webpack-dev-server 行为。
   devServer: {
